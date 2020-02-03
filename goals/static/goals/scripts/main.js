f28997e9d0a99baf0cee6969c4d32ef835011fd7
Matt <mfarncomb101@gmail.com>
2020-02-03 23:46:32 +1100

    Broke the hide/unhide functionality to make it more versatile. Still a WIP.

--- goals/static/goals/scripts/main.js
+++ goals/static/goals/scripts/main.js
@@ -10,6 +10,8 @@ document.addEventListener('DOMContentLoaded', () => {
 
         const title_btns = document.querySelectorAll('.title-btn'); // each individual title_btn
 
+        let goal_arr = []
+
         //indent all goals to their corresponding depth
         for (const goal of inserted_goals) {
             const goal_class = goal.className;
@@ -24,13 +26,34 @@ document.addEventListener('DOMContentLoaded', () => {
             item.style.gridColumnStart = item_depth;
             item.style.gridColumnEnd = item_depth+5;
 
-            console.log("item_depth:" + item_depth);
+            let item_id = item.id.split(' ')[1];
+            item_id = item_id.split('-')[1];
+            //console.log("id:" + item_id);
+
+            let parent = item.className.split(' ')[1];
+            let parentId = parent.split('_id_')[1];
+            //console.log("parent_id:" + parentId);
+
             /* if (item_depth != 2) {
                 item.style.display = "none";
             } */
-            
-            
-        }   
+
+            let new_goal = new Goal(item_id, parentId)
+            goal_arr.push(new_goal)    
+        }
+        for (parent of goal_arr) {
+            for (child of goal_arr) {
+                if (parent.id === child.parentId) {
+                    parent.addChild(child);
+                    
+                }
+            }
+            // prints out contents of goal class array
+            console.log("Children of " + parent.id + " are: ");
+                    for (child of parent.children) {
+                        console.log(child.id);
+                    } 
+        }
 
         // reveals form to add new goal
         for (const button of add_goal_buttons) {
@@ -47,6 +70,7 @@ document.addEventListener('DOMContentLoaded', () => {
             let id = btn.id.split('_')[1]; 
             
             console.log("Clicked id is:" + id);
+            let closing = false;
 
             //show all things that have the parent id matching this id
             id_arr = [];
@@ -61,14 +85,19 @@ document.addEventListener('DOMContentLoaded', () => {
                     id_arr.push(child_id);
                     console.log(id_arr);
                  }
+
+                 if (item.style.display != "none") {
+                    closing = true;
+                    console.log("true");
+                 }
+                 
                 console.log("my child is:" + child_id);
                 id_arr.forEach(e => {
                     if (id == e) {
-                        if (item.style.display != "none") {
-                            item.style.display = "none"; 
+                        if (closing) {
+                            item.style.display = "none";
                         }
                         else {
-                            console.log("was none")
                             item.style.display = "block";
                         }   
                     }
@@ -81,5 +110,46 @@ document.addEventListener('DOMContentLoaded', () => {
 //TODO: Function currenlty hides all children
 // - BUT if a child of a child is already hidden and then the parents parent is hidden...
 // - then th child of the child will be reveealed - because function basically does the opposite of its current state
+
+// - THe above error will be attempted to resolve with the class below - IN PROGRESS
+// Each goal will be a class
+// Each class will have an opened or close status
+// When it is closed individually, status is set to closed
+// When its parent is closed, all children will be closed by js and css only
+// The class will remain as open
+// This is so when the parent is reopened, then any previously oepened children can be reopened automatically
+/// This might be terrible and not wok, but havne timplemented it yet
+// Could even have a seperate fields of "was closed" and "is closed"
+
+    class Goal {
+
+        id;
+        parentId;
+        openStatus;
+        children;
+
+        constructor(id, parentId) {
+            this.id = id;
+            this.parentId = parentId
+            this.openStatus = false;
+            this.children = []
+        }
+
+        close() {
+            this.openStatus = false;
+        }
+
+        open() {
+            this.openStatus = true;
+        }
+
+        addChild(newChild) {
+            this.children.push(newChild)
+        }
+
+        removeChildren(child) {
+            //function to remove child
+        }
+    }
 
 
