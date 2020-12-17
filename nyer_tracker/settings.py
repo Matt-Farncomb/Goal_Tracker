f455fbfea5ac747e36b53e1f84e0a4639c029452
Matthew Farncomb <mfarn99@global.dbs.tv>
2020-12-17 11:56:57 +1100

    bugfixing

--- nyer_tracker/settings.py
+++ nyer_tracker/settings.py
@@ -148,7 +148,7 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 # Extra places for collectstatic to find static files.
 STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, 'static'),
+    os.path.join(BASE_DIR, 'static/styles'),
 )
 
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
